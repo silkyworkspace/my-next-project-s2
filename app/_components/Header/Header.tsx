2cc6832e67eb90b43b3328605bf3f2114a70088d
s0215 <s0215@administratornoMac-mini.local>
2025-12-08 14:32:34 +0900

    chapter4でNotFoundページ作成

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import styles from "./Header.module.css";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <a href="/" className={styles.logoLink}>
+            <Link href="/" className={styles.logoLink}>
                 <Image src="/logo.svg" alt="SIMPLE" className={styles.logo} width={348} height={133} priority />
-            </a>
+            </Link>
+            <nav>
+                <ul className={styles.items}>
+                    <li>
+                        <Link href="/members">メンバー</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 }
